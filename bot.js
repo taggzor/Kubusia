f22849d5e84b566d0c8d72f7a3f3586d5eff8038
taggzor <multimaxtroll@gmail.com>
2020-01-09 18:47:15 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,89 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const yts = require('yt-search');
+const ytplayer = require("yt-player");
+const YTDL = require("ytdl-core");
+
+var prefix = ".";
+let servers = {};
+
+
+
+
+function play(conn,msg){
+    var server = servers[parseInt(msg.guild.id)];
+    server.dispatcher = conn.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+    server.queue.shift();
+    server.dispatcher.on("end",function(){
+        if(server.queue[0]) play(conn,msg);
+        else conn.disconnect();
+    })
+}
+
+
+
+//const player = new ytplayer(yttoken);
+client.on("message", function(msg){
+    if(!msg.content.startsWith(prefix))return;
+    let command = msg.content.split(" ")[0].replace(prefix, "").toLowerCase();
+    let args = msg.content.split(" ").slice(1);
+    
+    if(command=="elo"){
+        msg.channel.send("No elo");
+    }
+    if(command=="play"){
+        
+        if(!msg.member.voiceChannelID){
+            msg.channel.send("Musisz dołączyć do kanału głosowego");
+            return;
+        }
+
+        if(!args[0]){
+            msg.channel.send("Kurwa play co?");
+            return;
+        }
+        var gildia=parseInt(msg.guild.id);
+        if(!servers[gildia]){
+            servers[gildia] = {
+                queue: []
+            };
+        }
+        var server = servers[parseInt(msg.guild.id)];
+        if(!server.queue[0])server.queue.push("https://www.youtube.com/watch?v=Vbks4abvLEw");
+        if(!args[0].startsWith("https://www.y")){
+            
+            yts(args.join(" "),function(err,r){
+                const video = r.videos 
+                args[0]=String(video[0].url);
+                server.queue.push(args[0]);
+            });
+        }
+
+        
+        if(args[0].startsWith("https://www.y")) server.queue.push(args[0]);
+        
+        if(!msg.guild.voiceConnection){
+            msg.member.voiceChannel.join()
+            .then(function(conn){
+                play(conn,msg);
+            })
+        }
+    }
+    if(command=="skip"){
+        var server = servers[parseInt(msg.guild.id)];
+        if(server.dispatcher) server.dispatcher.end();
+    }
+    if(command=="stop"){
+        var server = servers[parseInt(msg.guild.id)];
+        server.queue = [];
+        console.log(server.queue[0]);
+        if(msg.guild.voiceConnection)msg.guild.voiceConnection.disconnect();
+    }
+
+});
+
+
+
+
+client.login("NjY0ODQzMjM0NjM5MzQ3NzM0.Xhc9lg.2JSdQolBso9ysyUZpaQo8uTz_B0");
